9de83444363e3444b8d893117c072aaa6bddaeab
Madhur Bhargava <madhur.bhargava333@gmail.com>
2021-04-03 22:47:14 +0200

    Create lc_215_kth_largest_element_in_an_array.java

---
+++ lc_215_kth_largest_element_in_an_array.java
@@ -0,0 +1,32 @@
+/*
+LC-215 - Kth Largest Element in an Array
+Given an integer array nums and an integer k, return the kth largest element in the array.
+Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+Example 1:
+Input: nums = [3,2,1,5,6,4], k = 2
+Output: 5
+
+Example 2:
+Input: nums = [3,2,3,1,2,4,5,5,6], k = 4
+Output: 4
+
+Constraints:
+
+1 <= k <= nums.length <= 104
+-104 <= nums[i] <= 104
+*/
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> heap = new PriorityQueue<Integer>((a, b) -> b - a); //max heap
+        for(int num: nums) {
+            heap.add(num);
+        }
+        
+        int result = -1;
+        for(int i = 0; i < k; i++) {
+            result = heap.poll();
+        }
+        return result;
+    }
+}
